a62dc659f5f905178fcb731c14ccb4cd60de19a6
Natsuki0117 <natsu000117@icloud.com>
2026-08-25 13:07:51 +0900

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by 金井菜津希 on 2026/08/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
